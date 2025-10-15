063befbd6388a781c49d6cd5e31a43d5f5a5d1dd
Rizatя <example@mail.com>
2025-10-16 02:16:52 +0500

    на свой проект

--- shanyraq_project/wsgi.py
+++ shanyraq_project/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shanyraq_project.settings')
 
 application = get_wsgi_application()
 
-path = '/home/path/to/project'
+path = '/home/rizat/shanyraq'
 if path not in sys.path:
     sys.path.append(path)
 
